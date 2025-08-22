4ec89d2ec6682cfbe491a16869d1e784ade21172
Jayanth Samala <jayanthsamala@mac.lan>
2025-08-21 19:02:49 -0700

    fix blank screen + update package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 export default function Game() {
-  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
  function handlePlay(nextSquares) {
